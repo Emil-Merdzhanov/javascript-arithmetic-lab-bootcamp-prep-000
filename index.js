b2dcc6b517e559ea3a3f6a459b8864d09d728c86
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-23 20:05:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,66 @@
+//add
+var a = 2,
+    b = 3;
+function add(a,b) {
+  return a + b; 
+}
+console.log(add());
+
+
+//subtract
+var a = 10,
+    b = 3;
+function subtract(a,b) {
+  return a - b; 
+}
+console.log(subtract());
+
+
+//multiply
+var a = 10,
+    b = 3;
+function multiply(a,b) {
+  return a * b; 
+}
+console.log(multyply());
+
+//devide
+var a = 10,
+    b = 2;
+function divide(a, b) {
+  return a / b; 
+}
+console.log(devide());
+
+
+
+//increments
+var a = 10
+    b = 5
+function inc(n) {
+  return ++n
+}
+
+console.log(inc());
+
+
+
+
+//decrements
+var a = 10
+    b = 5
+function dec(n) {
+  return --n
+}
+
+console.log(dec());
+
+//makeInt(n)
+
+var a = 45.555
+    b = 10
+function makeInt(a,b) {
+  return parseInt("45.555", 10);
+}
+
+console.log(makeInt());
